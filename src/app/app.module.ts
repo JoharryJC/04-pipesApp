95c4127bfd2a6d0cceacee5b2c4fe38f68fb0c98
JoharryJC <energycuenta@yahoo.com>
2022-03-25 22:24:28 +0100

    primeNg FieldSet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRouterModule } from './app-router.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
+//Modulo personalizado
+import { AppRouterModule } from './app-router.module';
 import { SharedModule } from './shared/shared.module';
 import { VentasModule } from './ventas/ventas.module';
 
@@ -20,6 +22,7 @@ registerLocaleData(localeFr);
   ],
   imports: [
     BrowserModule, 
+    BrowserAnimationsModule, 
     AppRouterModule,
     SharedModule,
     VentasModule 
